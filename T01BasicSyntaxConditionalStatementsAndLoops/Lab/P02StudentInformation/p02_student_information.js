b3423313de8ff6b478afe805d591099110883b2d
georgi-rm <georgi.rm@gmail.com>
2023-02-21 21:41:21 +0200

    Problem 2 "Student Information" from Lab: Basic Syntax Conditional Statements And Loops

---
+++ T01BasicSyntaxConditionalStatementsAndLoops/Lab/P02StudentInformation/p02_student_information.js
@@ -0,0 +1,9 @@
+function studentInformation(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+studentInformation('John', 15, 5.54678);
+console.log("-------------------------");
+studentInformation('Steve', 16, 2.1426);
+console.log("-------------------------");
+studentInformation('Marry', 12, 6.00);
