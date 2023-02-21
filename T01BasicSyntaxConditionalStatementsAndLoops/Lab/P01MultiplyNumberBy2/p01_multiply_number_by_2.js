236c1668ef5700f6c570140ad1fb78619f8d6f71
georgi-rm <georgi.rm@gmail.com>
2023-02-21 21:40:36 +0200

    Problem 1 "Multiply Number By 2" from Lab: Basic Syntax Conditional Statements And Loops

---
+++ T01BasicSyntaxConditionalStatementsAndLoops/Lab/P01MultiplyNumberBy2/p01_multiply_number_by_2.js
@@ -0,0 +1,9 @@
+function multiplyNumberBy2(num) {
+    console.log(num * 2);
+}
+
+multiplyNumberBy2(2);
+console.log("-------------------------");
+multiplyNumberBy2(5);
+console.log("-------------------------");
+multiplyNumberBy2(20);
