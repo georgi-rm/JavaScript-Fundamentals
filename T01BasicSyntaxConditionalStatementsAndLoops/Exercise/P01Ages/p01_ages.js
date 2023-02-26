3b8a02269062bf1278d56220a1ec19145a474e21
georgi-rm <georgi.rm@gmail.com>
2023-02-26 20:25:15 +0200

    Problem 1 "Ages" from Exercise: Basic Syntax Conditional Statements And Loops

---
+++ T01BasicSyntaxConditionalStatementsAndLoops/Exercise/P01Ages/p01_ages.js
@@ -0,0 +1,24 @@
+function ages(age) {
+
+    if (age < 0) {
+        console.log("out of bounds");
+    } else if (age <= 2) {
+        console.log("baby");
+    } else if (age <= 13) {
+        console.log("child");
+    } else if (age <= 19) {
+        console.log("teenager");
+    } else if (age <= 65) {
+        console.log("adult");
+    } else {
+        console.log("elder");
+    }
+}
+
+ages(20);
+console.log("-------------------------");
+ages(1);
+console.log("-------------------------");
+ages(100);
+console.log("-------------------------");
+ages(-1);
