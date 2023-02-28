b9f81e2f00f638700eee7107da6df94ca5dd6b26
georgi-rm <georgi.rm@gmail.com>
2023-02-28 19:59:17 +0200

    Problem 5 "Leap Year" from Exercise: Basic Syntax Conditional Statements And Loops

---
+++ T01BasicSyntaxConditionalStatementsAndLoops/Exercise/P05LeapYear/p05_leap_year.js
@@ -0,0 +1,16 @@
+function leapYear(year) {
+
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
+}
+
+leapYear(1984);
+console.log("-------------------------");
+leapYear(2003);
+console.log("-------------------------");
+leapYear(4);
+console.log("-------------------------");
+leapYear(2000);
