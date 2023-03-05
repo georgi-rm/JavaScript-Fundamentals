795f2dfdf9b956002c75f3da7f66ed62e1486ba2
georgi-rm <georgi.rm@gmail.com>
2023-03-05 22:20:20 +0200

    Problem 2 "Concatenate Names" from Lab: Data Types And Variables

---
+++ T02DataTypesAndVariables/Lab/P02ConcatenateNames/p02_concatenate_names.js
@@ -0,0 +1,9 @@
+function concatenateNames(firstName, lastName, delimiter) {
+    console.log(`${firstName}${delimiter}${lastName}`);
+}
+
+concatenateNames('John', 'Smith', '->');
+console.log("-------------------------");
+concatenateNames('Jan', 'White', '<->');
+console.log("-------------------------");
+concatenateNames('Linda', 'Terry', '=>');
