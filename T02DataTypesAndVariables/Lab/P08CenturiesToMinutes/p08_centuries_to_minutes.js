35e94cc183bd9276a1f54563a9b42f0b4cd1356a
georgi-rm <georgi.rm@gmail.com>
2023-03-08 20:30:55 +0200

    Problem 8 "Centuries To Minutes" from Lab: Data Types And Variables

---
+++ T02DataTypesAndVariables/Lab/P08CenturiesToMinutes/p08_centuries_to_minutes.js
@@ -0,0 +1,13 @@
+function centuriesToMinutes(centuries) {
+
+    const years = centuries * 100;
+    const days = Math.floor(years * 365.2422);
+    const hours = days * 24;
+    const minutes = hours * 60;
+
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
+}
+
+centuriesToMinutes(1);
+console.log("-------------------------");
+centuriesToMinutes(5);
