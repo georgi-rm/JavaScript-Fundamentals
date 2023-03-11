211f566c17dcea79264f02f81a9a1be87a76aea8
georgi-rm <georgi.rm@gmail.com>
2023-03-11 12:39:17 +0200

    Problem 2 "Chars To String" from Exercise: Data Types And Variables

---
+++ T02DataTypesAndVariables/Exercise/P02CharsToString/p02_chars_to_string.js
@@ -0,0 +1,9 @@
+function charsToString(firstSymbol, secondSymbol, thirdSymbol) {
+    console.log(`${firstSymbol}${secondSymbol}${thirdSymbol}`);
+}
+
+charsToString('a', 'b', 'c');
+console.log("-------------------------");
+charsToString('%', '2', 'o');
+console.log("-------------------------");
+charsToString('1', '5', 'p');
