85aa16ceeacfd9d83a1e1d5983b299d0d0b58287
georgi-rm <georgi.rm@gmail.com>
2023-03-12 11:37:09 +0200

    Problem 7 "Lower Or Upper" from Exercise: Data Types And Variables

---
+++ T02DataTypesAndVariables/Exercise/P07LowerOrUpper/p07_lower_or_upper.js
@@ -0,0 +1,11 @@
+function lowerOrUpper(character) {
+    if (character == character.toLowerCase()) {
+        console.log('lower-case');
+    } else {
+        console.log('upper-case');
+    }
+}
+
+lowerOrUpper('L');
+console.log("-------------------------");
+lowerOrUpper('f');
