53a8952086799bf14cfde39a31da032e4147caae
georgi-rm <georgi.rm@gmail.com>
2023-04-01 12:32:38 +0300

    Problem 2 "Math Power" from Lab: Functions

---
+++ T04Functions/Lab/P02MathPower/p02_math_power.js
@@ -0,0 +1,13 @@
+function mathPower(number, power) {
+    let result = 1;
+
+    for (let i = 0; i < power; i++) {
+        result *= number;
+    }
+
+    console.log(result);
+}
+
+mathPower(2, 8);
+console.log('-------------------------');
+mathPower(3, 4);
